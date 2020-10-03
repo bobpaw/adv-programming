e91161a5db198c06d46a8b17c4063137f6402475
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-10-02 20:46:51 -0500

    Renamed file

--- csv-reader/main.cc
+++ csv-reader/main.cc
@@ -3,7 +3,7 @@
 #include "csv.h"
 
 int main () {
-  csv::Collection life_expectancy("WPP2015_MORT_F07_1_LIFE_EXPECTANCY_0_BOTH_SEXES.csv");
+  csv::Collection life_expectancy("life_expectancy.csv");
   for (auto e : life_expectancy[17]) {
     std::cout << e << "\t";
   }
