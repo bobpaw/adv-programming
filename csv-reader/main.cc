905d5b0216e794a01785a8c1719940751e1b5c14
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-10-05 15:33:21 -0500

    Changed the file that main.cc opens

--- csv-reader/main.cc
+++ csv-reader/main.cc
@@ -3,9 +3,9 @@
 #include "csv.h"
 
 int main () {
-  csv::Collection life_expectancy("life_expectancy.csv");
+  csv::Collection life_expectancy("life_expectancy_asc.csv");
   for (auto e : life_expectancy[17]) {
-    std::cout << e << "\t";
+      std::cout << e << "\t";
   }
   std::cout << std::endl;
 }
